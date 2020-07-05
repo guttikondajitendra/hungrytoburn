57cf23c55c4fdf271705f2cadb754cfbacb2692e
JitendraGuttikonda <guttikondajitendra@gmail.com>
2020-07-05 16:42:28 -0400

    Updates

---
+++ precache-manifest.9a4cc15e395f8b52a93259b48a71e451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "898b11fff2be51c7b0bd09372b751a05",
+    "url": "/hungrytoburn/index.html"
+  },
+  {
+    "revision": "125f28e17a7bf831cb64",
+    "url": "/hungrytoburn/static/css/main.1c9c0e26.chunk.css"
+  },
+  {
+    "revision": "f6a847a4af5fb792a7c6",
+    "url": "/hungrytoburn/static/js/2.720d9380.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/hungrytoburn/static/js/2.720d9380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "125f28e17a7bf831cb64",
+    "url": "/hungrytoburn/static/js/main.71425222.chunk.js"
+  },
+  {
+    "revision": "5ef6f897616501b58677",
+    "url": "/hungrytoburn/static/js/runtime-main.32354610.js"
+  }
+]);
